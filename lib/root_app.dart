743b5006420e527331c93159a6b5240a56fd8283
jimmyriguelle <jimmyriguelle@hotmail.com>
2021-12-26 23:19:12 +0100

    bottom navBar

--- lib/root_app.dart
+++ lib/root_app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/pages/home_page.dart';
+import 'package:food_app/theme/colors.dart';
+import 'package:line_icons/line_icons.dart';
 
 class RootApp extends StatefulWidget {
   const RootApp({Key? key}) : super(key: key);
@@ -8,8 +11,76 @@ class RootApp extends StatefulWidget {
 }
 
 class _RootAppState extends State<RootApp> {
+  int activeTab = 0;
+
+  List activeTabs = [
+    LineIcons.home,
+    LineIcons.barcode,
+    LineIcons.comment,
+    LineIcons.user
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: IndexedStack(
+        index: activeTab,
+        children: const <Widget>[
+          HomePage(),
+          Center(
+            child: Text(
+              'Product List',
+              style: TextStyle(fontSize: 35),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Chat',
+              style: TextStyle(fontSize: 35),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Account',
+              style: TextStyle(fontSize: 35),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(color: white),
+        width: MediaQuery.of(context).size.width,
+        height: 90,
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 10, bottom: 15, left: 15, right: 15),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadiusDirectional.circular(30),
+                color: Colors.black),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 20, right: 20, bottom: 10, top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: List.generate(activeTabs.length, (index) {
+                  return IconButton(
+                    onPressed: () {
+                      setState(() {
+                        activeTab = index;
+                      });
+                    },
+                    icon: Icon(
+                      activeTabs[index],
+                      color: Colors.white,
+                      size: 25,
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
