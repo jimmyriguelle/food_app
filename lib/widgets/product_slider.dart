76f6699e556278819e94ca9c2b33ca4f53422a9c
jimmyriguelle <jimmyriguelle@hotmail.com>
2021-12-26 22:35:53 +0100

    Initial commit

---
+++ lib/widgets/product_slider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ActiveDot extends StatelessWidget {
+  const ActiveDot({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class InactiveDot extends StatelessWidget {
+  const InactiveDot({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class ProductSlider extends StatefulWidget {
+  const ProductSlider({Key? key}) : super(key: key);
+
+  @override
+  _ProductSliderState createState() => _ProductSliderState();
+}
+
+class _ProductSliderState extends State<ProductSlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
